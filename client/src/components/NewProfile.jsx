e78b60fd9bec65047d608de596d84162afba0207
samlui1009 <sam_lui@hotmail.ca>
2025-07-24 23:42:47 -0700

    CSS stylings applied for new profile demographic inputs, plus useStates for displaying two separate forms

--- client/src/components/NewProfile.jsx
+++ client/src/components/NewProfile.jsx
@@ -1,11 +1,51 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './NewProfile.css';
 import { GrFormNextLink } from "react-icons/gr";
 
 function NewProfile() {
 
+    const [user, setUser] = useState(null);
+    const [displayUserForm, setDisplayUserForm] = useState(true)
     const [healthGoalsPanel, setHealthGoalsPanel] = useState(false);
     // States to show the panel for health goals
+    const [userForm, setUserForm] = useState({
+        name:"",
+        age:"",
+        weight:"",
+        gender:""
+        // Initial states for first form, just like how it's set in UserProfile
+    })
+
+    // This will be used to handle submissions for the "first" form that shows up
+    const handleFirstFormSubmit = (e) => {
+        e.preventDefault();
+
+        fetch('http://localhost:8080/api/user/setup-user/${id}', {
+            method:'PUT',
+            headers: {
+                'Content-Type':'application/json',
+            },
+            body: JSON.stringify(userForm),
+            })
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error("Failed to update this new users' demographics!");
+                }
+                return res.json();
+            })
+            .then((updatedUserData) => {
+                setUser(updatedUserData);
+                setUserForm({
+                    name:"",
+                    age:"",
+                    weight:"",
+                    gender:""
+                })
+            })
+            .catch((err) => {
+                console.error("Error in updating this new users' demographics!", err);
+            })
+        };
 
     return(
         <div>
@@ -16,28 +56,46 @@ function NewProfile() {
                     <h3>First things first, tell me a little bit about yourself!</h3>
                     <form className="general-info-form">
                         <label className="form-labels">What's your name?</label>
-                        <input type="text" className="form-inputs"></input>
+                        <input type="text" 
+                               className="form-inputs"
+                               value={userForm.name}
+                               onChange={(e) => setUserForm({ ...userForm, name: e.target.value})}></input>
                         <label className="form-labels">What's your age?</label>
-                        <input type="number" className="form-inputs"></input>
+                        <input type="number" 
+                               className="form-inputs"
+                               value={userForm.age}
+                               onChange={(e) => setUserForm({ ...userForm, age: e.target.value})}></input>
                         <label className="form-labels">What's your current weight in kilograms?</label>
-                        <input type="number" className="form-inputs"></input>
+                        <input type="number" 
+                               className="form-inputs"
+                               value={userForm.weight}
+                               onChange={(e) => setUserForm({ ...userForm, weight: e.target.value})}></input>
                         <label className="form-labels">What are your preferred pronouns?</label>
-                        <input type="text" className="form-inputs"></input>
+                        <input type="text" 
+                               className="form-inputs"
+                               value={userForm.gender}
+                               onChange={(e) => setUserForm({ ...userForm, gender: e.target.value})}></input>
                     </form>
-                    <p>Next</p>
-                    <button className="next-btn"><GrFormNextLink></GrFormNextLink></button>
+                    <input type="button" className="next-btn"
+                            value="Next" 
+                            onClick={(e) => {
+                                handleFirstFormSubmit(e); 
+                                setHealthGoalsPanel(true);}}></input>
                 </div>
 
-                <h3>Great! Now, tell me about your current health goals.</h3>
-                <form>
-                    <label>What is the daily intake limit for calories for beverages that you want to set?</label>
-                    <input type="number"></input>
-                    <label>What is the daily intake limit for sugar (in grams) for beverages that you want to set?</label>
-                    <input type="number"></input>
-                    <label>What is the daily intake limit for caffeine (in milligrams) for beverages that you want to set?</label>
-                    <input type="number"></input>
-                </form>
-                {/* This form needs to be part of a conditional/useState */}
+                {healthGoalsPanel && (
+                    <div className="health-goals-panel">
+                        <h3>Great! Now, tell me about your current health goals.</h3>
+                        <form>
+                            <label>What is the daily intake limit for calories for beverages that you want to set?</label>
+                            <input type="number"></input>
+                            <label>What is the daily intake limit for sugar (in grams) for beverages that you want to set?</label>
+                            <input type="number"></input>
+                            <label>What is the daily intake limit for caffeine (in milligrams) for beverages that you want to set?</label>
+                            <input type="number"></input>
+                        </form>
+                    </div>
+                )}
             </div>
         </div>
     );
