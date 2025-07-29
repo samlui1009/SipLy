7346a4b5433b6010c2f76c40f147a4190b0feb75
samlui1009 <sam_lui@hotmail.ca>
2025-07-28 22:17:30 -0700

    Basic prompt applied, proper response is generated - requires more fine tuning

--- backend/src/main/java/com/siply/backend/AIController.java
+++ backend/src/main/java/com/siply/backend/AIController.java
@@ -1,10 +1,15 @@
 package com.siply.backend;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatModel;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -21,14 +26,23 @@ import java.util.Map;
 public class AIController {
 
     private final OpenAiChatModel chatModel;
+    // We need a Chat client
 
     @Autowired
     public AIController(OpenAiChatModel chatModel) {
         this.chatModel = chatModel;
     }
 
-    @GetMapping("/generate")
-    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-        return Map.of("generation", chatModel.call(message));
+    @GetMapping("/bev-bot-analysis")
+    public String generateResponse(@RequestParam(defaultValue = "What should I drink?") String message) {
+        String promptText = """
+                Provide the user with some suggestions on what they can drink to stay healthy and avoid sugar and caffeine.
+                Only return the rewritten sentence. Do not include explanations.
+                """.formatted(message);
+        Prompt prompt = new Prompt(new UserMessage(promptText));
+        ChatResponse response = chatModel.call(prompt);
+        String result = response.getResult().getOutput().getText();
+        return result;
+        // What in tarnation is this
     }
 }
